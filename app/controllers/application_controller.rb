0fe31701b73666308661630af896caccf0f32449
helenavocado <hwang92@uchicago.edu>
2021-11-07 22:06:33 +0000

    ksjdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    render({:template => "/user_views/index.erb.html"})
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
-
+get("/", { :controller => "application" , :action => "index"})
+get("/users", { :controller => "users" , :action => "index"})
+get("/users/:username", {:controller => "users", :action=> "detail"})
 end
