314edf24d4a6d04388670def2472b577798d1406
helenavocado <hwang92@uchicago.edu>
2021-11-12 21:27:06 +0000

    20211112 427pm

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,4 +12,49 @@ class PhotosController < ApplicationController
     
     render({:template => "photo_views/detail.html.erb"})
   end
+
+  def delete
+    the_id = params.fetch("photoid")
+    matching_photo = Photo.where({:id => the_id})
+    the_photo = matching_photo.at(0)
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def insert
+    #Parameters: {"inputurl"=>"http://3.bp.blogspot.com/-UXhI6bPgGQ4/UpFrgHjhriI/AAAAAAAAWS8/Do-VZZwv7HM/s1600/bernese+mountain+dog+.jpg", "inputcaption"=>"dsfsa", "inputowner"=>"117"}
+    input_image = params.fetch("inputurl")
+    input_caption = params.fetch("inputcaption")
+    input_owner_id = params.fetch("inputowner")
+
+    new_photo = Photo.new
+    new_photo.image = input_image
+    new_photo.caption = input_caption
+    new_photo.owner_id = input_owner_id
+    
+    new_photo.save
+    redirect_to("/photos/#{new_photo.id}")
+  end
+  
+  def update
+    
+    photoid = params.fetch("photoid")
+    the_photo = Photo.where({:id => photoid})
+    photo = the_photo.at(0)
+
+    caption = params.fetch("input_caption")
+    image = params.fetch("input_image")
+    
+    
+    
+    photo.image = image
+    photo.caption = caption
+    photo.save
+    # render({:template => "photo_views/test.html.erb"})
+
+    redirect_to("/photos/#{photoid}")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ get("/users", { :controller => "users" , :action => "index"})
 get("/users/:username", {:controller => "users", :action=> "detail"})
 get("/photos", { :controller => "photos" , :action => "index"})
 get("/photos/:photoid", { :controller => "photos" , :action => "detail"})
-
+get("/delete_photo/:photoid", {:controller => "photos", :action => "delete"})
+get("/insert_photo_record", {:controller => "photos", :action => "insert"})
+get("/update_photo/:photoid", {:controller => "photos", :action => "update"})
+get("/insert_comment_record/:photoid", {:controller => "photos", :action => "update"})
 end
